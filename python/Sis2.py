47f777cb16e954c405ee04d8831ed66a144862de
simoneserafini <simone.serafini.87@gmail.com>
2016-03-25 16:15:08 +0100

    Added the new sis library

---
+++ python/Sis2.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 10:31:05 2016
+
+@author: elenaiseni
+
+"""
+
+import numpy as np
+
+
+def readsis(filename):
+    ''' Legge i file .sis con cui mi hanno passato i dati
+    
+    Parameters
+    ----------
+    filename : stringa con il nome o il path relativo del file.
+    
+    Returns im1, im2, image, rawdata, width, height
+    -------
+    im1 : ndarray 2D
+        la prima meta dell'immagine [righe 0 : height/2-1].
+    
+    im2 : ndarray 2D
+        la prima meta dell'immagine [righe height/2 : height-1].
+    
+    image : ndarray 2D
+        l'immagine completa. 
+        
+    rawdata : ndarray 1D
+        raw data letti dal file
+    
+    ### suppressed (height, width) : tuple
+        dimensioni dell'array image
+    
+    Notes
+    -----
+    NB tutti gli output sono finestre (slices) sui raw data: ogni modifica cambierà anche gli elementi corrispondenti in rawdata
+    '''
+    f = open(filename, 'rb')  #apre in binario
+    rawdata = np.fromfile(f,'H').astype(int)
+    # 'H' = uint16
+    # lo trovo in np.typeDict
+    #legge in array i dati formattati uint16 e li casta ad int
+    ''' NB è importantissimo castarli ad int:
+        visto che sono numeri molto grandi, gli unsigned short non vanno bene (overflow) '''
+    f.close
+    
+    width=rawdata[6]  # N cols
+    height=rawdata[5] # N rows
+    #rispetto ad octave, gli indici cambiano (python è 0-based)
+
+    image = rawdata[-width*height : ]
+    image.resize(height,width)
+           
+    #in matlab, poiché reshape legge per colonne, era necessario scambiare gli indici e poi trasporre la matrice. Ora non più
+    
+    im0 = image[:height//2, :]
+    im1 = image[height//2:, :]
+    
+    
+    return im0, im1, image, rawdata #, image.shape
+    
+def sis_write(self, image, filename):
+        """
+        Low-level interaction with the sis file for writing it.
+        Writes the whole image, with the unused part filled with zeros.
+
+        Args:
+            image (np.array): the 2d-array that must be writed after conversion
+            to 16-bit unsigned-integers (must be already normalized)
+            filename (string): sis filename
+        """
+        #keep the double-image convention for sis files, filling the unused
+        #with zeros
+        if self == 0:
+            image = np.concatenate((image, np.zeros_like(image)))
+        elif self == 1:
+            image = np.concatenate((np.zeros_like(image), image))
+
+        with open(str(filename), 'wb') as fid:
+            fid.write('0'*10) #skip unused
+
+            height, width = image.shape
+            size = np.array([height, width], dtype=np.uint16)
+            size.tofile(fid)
+
+            fid.write('0'*182)
+            fid.write('0'*4) #skip offset
+
+            image.astype(np.uint16).tofile(fid)
