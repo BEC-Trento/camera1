ae7eff9e3be36c83c97852baf646884ac4872692
simoneserafini <simone.serafini.87@gmail.com>
2016-03-29 15:55:41 +0200

    Completed the reading and writing library

--- python/libraries/Sis2.py
+++ python/libraries/Sis2.py
@@ -1,61 +1,102 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 25 2016
 
-@author: SimoneSerafini
-
-"""
+# Copyright (C) 2016  Simone Serafini
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
 import time
 import datetime
 
-
 def readsis(filename):
-    ''' Legge i file .sis con cui mi hanno passato i dati
+    ''' Read sis files, both old version and SisV2
     Parameters
     ----------
     filename : stringa con il nome o il path relativo del file.
-    Returns im1, im2, image, rawdata, width, height
+    Returns im1, im2, image, rawdata, stringa, block
     -------
     im1 : ndarray 2D
-        la prima meta dell'immagine [righe 0 : height/2-1].
+        first half of the image [righe 0 : height/2-1].
     im2 : ndarray 2D
-        la prima meta dell'immagine [righe height/2 : height-1].
+        second half of the image [righe height/2 : height-1].
     image : ndarray 2D
-        l'immagine completa.
+        whole image
     rawdata : ndarray 1D
-        raw data letti dal file
-    ### suppressed (height, width) : tuple
-        dimensioni dell'array image
+        raw data read from file
+    stringa : string
+        comment and datestamp of the image
+    block : tuple (Bheight,Bwidth)
+        Bheight : int
+            y dimension of the sub-block
+        Bwidth : int
+            x dimension of the sub-block
     Notes
     -----
-    NB tutti gli output sono finestre (slices) sui raw data: ogni modifica cambierà anche gli elementi corrispondenti in rawdata
+    NB all the outputs are slices of the raw data: any modifications of them will be reflected also in the linked rawdata elements
     '''
-    f = open(filename, 'rb')  #apre in binario
+    f = open(filename, 'rb')        # open in reading and binary mode
+    header = f.read(512)            # read the fixed size header: 512 bytes
+    if header[0] == 48:             # 48 is ASCII code for '0': all older sis start with 0
+        sisVersion = 'sis1'
+        datestamp = 'we do not know'
+        comment = 'nothing'
+    elif header[0:5] == b'SisV2':   # SisV2 print header
+        sisVersion = str(header[0:5])
+        datestamp = str(header[18:37])
+        comment = str(header[37:56])
+    else:
+        print('What fuck are you opening?')
+        return True
+
+    print("You are opening the " + sisVersion[2:-1] + " file: " + filename)
+    print("It says: " + comment[1:])
+    print("...and was created on " + datestamp[1:])
+    f.close
+    # Sometimes it gives error if the file is opened a sigle time
+
+    f = open(filename, 'rb')                        # open in reading and binary mode
     rawdata = np.fromfile(f,'H').astype(int)
     # 'H' = uint16
-    # lo trovo in np.typeDict
-    #legge in array i dati formattati uint16 e li casta ad int
-    ''' NB è importantissimo castarli ad int:
-        visto che sono numeri molto grandi, gli unsigned short non vanno bene (overflow) '''
+    # types are listed in np.typeDict
+    # put in an array the data formatted uint16 and casted to int
+    ''' NB fundamental to cast to int:
+        unsigned short gives overflow '''
     f.close
 
+    # Dimension of the whole image
     width=rawdata[6]  # N cols
     height=rawdata[5] # N rows
-    #rispetto ad octave, gli indici cambiano (python è 0-based)
 
-    image = rawdata[-width*height : ]
+    # Dimension of the sub-blocks
+    Bwidth=rawdata[8]  # N subcols
+    Bheight=rawdata[7] # N subrows
+
+    # Reading the images
+    image = rawdata[-width*height:]
     image.resize(height,width)
 
-    #in matlab, poiché reshape legge per colonne, era necessario scambiare gli indici e poi trasporre la matrice. Ora non più
-
+    # defines the two images in the sis
     im0 = image[:height//2, :]
     im1 = image[height//2:, :]
 
-    return im0, im1, image, rawdata #, image.shape
+    stringa = sisVersion[1:-1] + ' ' + datestamp[2:-1] + ' ' + comment[2:]
+    block = (Bheight,Bwidth)
 
-def sis_write(self, image, filename, stamp):
+    return im0, im1, image, rawdata, stringa , block
+
+def sis_write(self, image, filename, Bheight, Bwidth, stamp):
         """
         Low-level interaction with the sis file for writing it.
         Writes the whole image, with the unused part filled with zeros.
@@ -64,6 +105,9 @@ def sis_write(self, image, filename, stamp):
             image (np.array): the 2d-array that must be writed after conversion
             to 16-bit unsigned-integers (must be already normalized)
             filename (string): sis filename
+            Bheight (int): the y dimension of the eventual block
+            Bwidth (int): the x dimension of the eventual block
+            stamp (string): a string to describe who, why and what you want
         """
         #keep the double-image convention for sis files, filling the unused
         #with zeros
@@ -83,7 +127,6 @@ def sis_write(self, image, filename, stamp):
             size.tofile(fid)
 
             # Here we put 2*2 more bytes with the sub-block dimension
-            Bheight, Bwidth = 300, 200
             Bsize = np.array([Bheight, Bwidth], dtype=np.uint16)
             Bsize.tofile(fid)
 
@@ -92,10 +135,9 @@ def sis_write(self, image, filename, stamp):
             phead = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
             fid.write(phead.encode())
 
-            # More?
+            # More: descriptive stamp
             fid.write(stamp.encode())
             freeHead = '0'*(475-len(stamp))
-            #freeHead = '0'*163
             fid.write(freeHead.encode())
 
             image.astype(np.uint16).tofile(fid)
